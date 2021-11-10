d83ab7fffb8fd32628c0838d1d4887d63156e431
peachferris <52829920+peachferris@users.noreply.github.com>
2021-11-10 17:08:03 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -38,11 +38,11 @@ $(document).ready(function(){
 
 $(document).ready(function () {
     $('.slider').slick({
-        arrows: true,
-        dots: true,
+        arrows: false,
+        dots: false,
         slidesToShow: 1,
         autoplay: true,
-        speed: 1000,
+        speed: 2000,
         autoplaySpeed: 800,
         responsive: [
             {
@@ -55,9 +55,45 @@ $(document).ready(function () {
                 breakpoint: 550,
                 settings: {
                     slidesToShow: 1
+                    
                 }
             }
         ]
     });
 });
 
+let stars = document.getElementById("stars");
+let moon = document.getElementById("moon");
+let mountains_behind = document.getElementById("mountains_behind");
+let mountains_front = document.getElementById("mountains_front");
+let text = document.getElementById("text");
+let btn = document.getElementById("btn");
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () =>  {
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + "px";
+    moon.style.top = value * 1.05 + "px";
+    mountains_behind.style.top = value * 0.5 + "px";
+    mountains_front.style.top = value * 0 + "px";
+    text.style.marginRight = value * 4 + "px";
+    text.style.marginTop = value * 1.5 + "px";
+    btn.style.marginTop = value * 1.5 + "px";
+    header.style.top = value * 0.5 + "px";
+});
+
+const openMenu = document.querySelector(".js-open-btn");
+const closeMenu = document.querySelector(".js-close-btn");
+const mobileMenu = document.querySelector(".list");
+
+openMenu.addEventListener("click", () => {
+    closeMenu.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+    openMenu.classList.toggle("hidden");
+});
+
+closeMenu.addEventListener("click", () => {
+    closeMenu.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+    openMenu.classList.toggle("hidden");
+});
